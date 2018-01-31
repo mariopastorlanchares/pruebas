bd2a8d30b361fdde3138e60513e53428bcb6286a
dam <dam@203P01>
2018-01-31 13:58:22 +0100

    SEGUNDA VERSION

---
+++ src/git/Calculadora.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package git;
+
+/**
+ *
+ * @author dam
+ */
+public class Calculadora {
+int n1;
+int n2;
+    public Calculadora(int a, int b) {
+        n1=a;
+        n2=b;
+    }
+    public int suma(){
+        return n1+n2;
+    }
+    
+}
