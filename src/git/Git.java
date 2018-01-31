db0a8689f6a847a305f557dedc71f011144eb741
dam <dam@203P01>
2018-01-31 13:57:01 +0100

    SEGUNDA VERSION

--- src/git/Git.java
+++ src/git/Git.java
@@ -11,11 +11,10 @@ package git;
  */
 public class Git {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+
+        Calculadora c=new Calculadora(4,2);
+        System.out.println(c.suma());
     }
     
 }
